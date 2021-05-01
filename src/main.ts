f45178a6ec857a2aacd5a94107be1696342a8a6e
william koller <wkoller25@gmail.com>
2021-05-01 03:14:00 -0300

    feat: add LoggingInterceptor and ModelNotFoundException

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { swaggerConfig } from '@/docs/swagger-config';
+import { LoggingInterceptor } from '@/common/interceptors/logging.interceptor';
+import { ModelNotFoundException } from '@/common/filters/model-not-found.exception.filter';
 
 const key = 'fetch';
 
@@ -15,6 +17,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new ModelNotFoundException());
   swaggerConfig(app);
   await app.listen(process.env.PORT, () => logger.log(`App running 🔥`));
 
